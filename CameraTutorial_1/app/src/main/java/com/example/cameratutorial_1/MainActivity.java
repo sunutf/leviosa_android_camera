1205d1b94a87cc596e15a1b10f556347bf7b3f64
DESKTOP-CUF2S4E\ggavy <sunutf@gmail.com>
2019-05-23 01:14:55 +0900

    [Done] capture photo with pitch/ roll angle labeling --- can not upload at gallery

---
+++ CameraTutorial_1/app/src/main/java/com/example/cameratutorial_1/MainActivity.java
@@ -0,0 +1,404 @@
+package com.example.cameratutorial_1;
+
+import com.example.cameratutorial_1.representation.Quaternion;
+
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.cameratutorial_1.representation.Quaternion;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
+
+    /**
+     * Constant specifying the factor between a Nano-second and a second
+     */
+    private static final float NS2S = 1.0f / 1000000000.0f;
+
+    /**
+     * The quaternion that stores the difference that is obtained by the gyroscope.
+     * Basically it contains a rotational difference encoded into a quaternion.
+     *
+     * To obtain the absolute orientation one must add this into an initial position by
+     * multiplying it with another quaternion
+     */
+    private final Quaternion deltaQuaternion = new Quaternion();
+
+
+    /**
+     * The time-stamp being used to record the time when the last gyroscope event occurred.
+     */
+    private long timestamp;
+
+    /**
+     * This is a filter-threshold for discarding Gyroscope measurements that are below a certain level and
+     * potentially are only noise and not real motion. Values from the gyroscope are usually between 0 (stop) and
+     * 10 (rapid rotation), so 0.1 seems to be a reasonable threshold to filter noise (usually smaller than 0.1) and
+     * real motion (usually > 0.1). Note that there is a chance of missing real motion, if the use is turning the
+     * device really slowly, so this value has to find a balance between accepting noise (threshold = 0) and missing
+     * slow user-action (threshold > 0.5). 0.1 seems to work fine for most applications.
+     *
+     */
+    private static final double EPSILON = 0.1f;
+
+    /**
+     * Value giving the total velocity of the gyroscope (will be high, when the device is moving fast and low when
+     * the device is standing still). This is usually a value between 0 and 10 for normal motion. Heavy shaking can
+     * increase it to about 25. Keep in mind, that these values are time-depended, so changing the sampling rate of
+     * the sensor will affect this value!
+     */
+    private double gyroscopeRotationVelocity = 0;
+
+    /**
+     * Temporary variable to save allocations.
+     */
+    private Quaternion correctedQuaternion = new Quaternion();
+    private Quaternion currentOrientationQuaternion;
+
+
+
+    final String TAG = getClass().getSimpleName();
+    ImageView imageView;
+    TextView Xaxis, Yaxis, Zaxis;
+    Button cameraBtn;
+    String mCurrentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    SensorManager m_sensorManager;
+    float []m_lastMagFields;
+    float []m_lastAccels;
+    private float[] m_rotationMatrix = new float[16];
+    private float[] m_remappedR = new float[16];
+    private float[] m_orientation = new float[4];
+    private Sensor gyroSensor;
+
+    /* fix random noise by averaging tilt values */
+    final static int AVERAGE_BUFFER = 30;
+    float []m_prevPitch = new float[AVERAGE_BUFFER];
+    float m_lastPitch = 0.f;
+    float m_lastYaw = 0.f;
+    /* current index int m_prevEasts */
+    int m_pitchIndex = 0;
+
+    float []m_prevRoll = new float[AVERAGE_BUFFER];
+    float m_lastRoll = 0.f;
+    /* current index into m_prevTilts */
+    int m_rollIndex = 0;
+
+    /* center of the rotation */
+    private float m_tiltCentreX = 0.f;
+    private float m_tiltCentreY = 0.f;
+    private float m_tiltCentreZ = 0.f;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 레이아웃과 변수 연결
+        imageView = findViewById(R.id.imageview);
+        cameraBtn = findViewById(R.id.camera_button);
+        Xaxis = findViewById(R.id.Xaxis);
+        Yaxis = findViewById(R.id.Yaxis);
+        Zaxis = findViewById(R.id.Zaxis);
+
+
+        m_sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        registerListeners();
+        // 카메라 버튼에 리스터 추가
+        cameraBtn.setOnClickListener(this);
+
+
+
+        // 6.0 마쉬멜로우 이상일 경우에는 권한 체크 후 권한 요청
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED ) {
+                Log.d(TAG, "권한 설정 완료");
+            } else {
+                Log.d(TAG, "권한 설정 요청");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
+
+    private void registerListeners() {
+        m_sensorManager.registerListener(this, m_sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_GAME);
+        m_sensorManager.registerListener(this, m_sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    private void unregisterListeners() {
+        m_sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        unregisterListeners();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPause() {
+        unregisterListeners();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        registerListeners();
+        super.onResume();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            accel(event);
+        }
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            mag(event);
+        }
+    }
+
+    private void accel(SensorEvent event) {
+        if (m_lastAccels == null) {
+            m_lastAccels = new float[3];
+        }
+
+        System.arraycopy(event.values, 0, m_lastAccels, 0, 3);
+
+        /*if (m_lastMagFields != null) {
+            computeOrientation();
+        }*/
+    }
+
+    private void mag(SensorEvent event) {
+        if (m_lastMagFields == null) {
+            m_lastMagFields = new float[3];
+        }
+
+        System.arraycopy(event.values, 0, m_lastMagFields, 0, 3);
+
+        if (m_lastAccels != null) {
+            computeOrientation();
+        }
+    }
+
+    Filter [] m_filters = { new Filter(), new Filter(), new Filter() };
+
+    private class Filter {
+        static final int AVERAGE_BUFFER = 10;
+        float []m_arr = new float[AVERAGE_BUFFER];
+        int m_idx = 0;
+
+        public float append(float val) {
+            m_arr[m_idx] = val;
+            m_idx++;
+            if (m_idx == AVERAGE_BUFFER)
+                m_idx = 0;
+            return avg();
+        }
+        public float avg() {
+            float sum = 0;
+            for (float x: m_arr)
+                sum += x;
+            return sum / AVERAGE_BUFFER;
+        }
+
+    }
+
+    private void computeOrientation() {
+        if (SensorManager.getRotationMatrix(m_rotationMatrix, null, m_lastAccels,m_lastMagFields )) {
+            SensorManager.getOrientation(m_rotationMatrix, m_orientation);
+
+            /* 1 radian = 57.2957795 degrees */
+            /* [0] : yaw, rotation around z axis
+             * [1] : pitch, rotation around x axis
+             * [2] : roll, rotation around y axis */
+            float yaw = m_orientation[0] * 57.2957795f;
+            float pitch = m_orientation[1] * 57.2957795f;
+            float roll = m_orientation[2] * 57.2957795f;
+
+            m_lastYaw = m_filters[0].append(yaw);
+            m_lastPitch = m_filters[1].append(pitch);
+            m_lastRoll = m_filters[2].append(roll);
+
+            Xaxis.setText("azi z: " + String.format("%.1f", m_lastYaw));
+            Yaxis.setText("pitch x: " + String.format("%.1f", m_lastPitch));
+            Zaxis.setText("roll y: " + String.format("%.1f", m_lastRoll));
+        }
+    }
+
+    // 권한 요청
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult");
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED ) {
+            Log.d(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
+        }
+    }
+
+    // 버튼 onClick리스터 처리부분
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.camera_button:
+                // 카메라 앱을 여는 소스
+                dispatchTakePictureIntent();
+                break;
+        }
+    }
+
+    // 카메라로 촬영한 영상을 가져오는 부분
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        try {
+            switch (requestCode) {
+                case REQUEST_TAKE_PHOTO: {
+                    if (resultCode == RESULT_OK) {
+                        File file = new File(mCurrentPhotoPath);
+                        Bitmap bitmap = MediaStore.Images.Media
+                                .getBitmap(getContentResolver(), Uri.fromFile(file));
+                        if (bitmap != null) {
+                            ExifInterface ei = new ExifInterface(mCurrentPhotoPath);
+                            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                                    ExifInterface.ORIENTATION_UNDEFINED);
+
+                            Bitmap rotatedBitmap = null;
+                            switch (orientation) {
+
+                                case ExifInterface.ORIENTATION_ROTATE_90:
+                                    rotatedBitmap = rotateImage(bitmap, 90);
+                                    break;
+
+                                case ExifInterface.ORIENTATION_ROTATE_180:
+                                    rotatedBitmap = rotateImage(bitmap, 180);
+                                    break;
+
+                                case ExifInterface.ORIENTATION_ROTATE_270:
+                                    rotatedBitmap = rotateImage(bitmap, 270);
+                                    break;
+
+                                case ExifInterface.ORIENTATION_NORMAL:
+                                default:
+                                    rotatedBitmap = bitmap;
+                            }
+
+                            imageView.setImageBitmap(rotatedBitmap);
+                            galleryAddPic();
+                        }
+                    }
+                    break;
+                }
+            }
+        } catch (Exception error) {
+            error.printStackTrace();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" +  String.format("%.1f", m_lastPitch) + "_" + String.format("%.1f", m_lastRoll) + "_";
+        ///        File storageDir = new File(Environment.getExternalStorageDirectory() + "/blackJin/");
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.cameratutorial_1.fileprovider",
+                        photoFile);
+
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+            }
+        }
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+    private void galleryAddPic(){
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+
+//        Intent galleryIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+//        File f = new File(mCurrentPhotoPath);
+//        Uri picUri = Uri.fromFile(f);
+//        galleryIntent.setData(picUri);
+//        this.sendBroadcast(galleryIntent);
+    }
+
+
+}
+
+
